8e19838a29ed8e80b2384dbd8d2a17b0a11aab1b
xietianfu <826597263@qq.com>
2017-08-09 21:39:02 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace AddYamdiFlv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtIn = new System.Windows.Forms.TextBox();
+            this.btnIn = new System.Windows.Forms.Button();
+            this.btnOut = new System.Windows.Forms.Button();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "输入路径";
+            // 
+            // txtIn
+            // 
+            this.txtIn.Location = new System.Drawing.Point(103, 18);
+            this.txtIn.Name = "txtIn";
+            this.txtIn.Size = new System.Drawing.Size(298, 21);
+            this.txtIn.TabIndex = 1;
+            // 
+            // btnIn
+            // 
+            this.btnIn.Location = new System.Drawing.Point(422, 18);
+            this.btnIn.Name = "btnIn";
+            this.btnIn.Size = new System.Drawing.Size(75, 23);
+            this.btnIn.TabIndex = 2;
+            this.btnIn.Text = "浏览";
+            this.btnIn.UseVisualStyleBackColor = true;
+            this.btnIn.Click += new System.EventHandler(this.btnIn_Click);
+            // 
+            // btnOut
+            // 
+            this.btnOut.Location = new System.Drawing.Point(422, 62);
+            this.btnOut.Name = "btnOut";
+            this.btnOut.Size = new System.Drawing.Size(75, 23);
+            this.btnOut.TabIndex = 5;
+            this.btnOut.Text = "浏览";
+            this.btnOut.UseVisualStyleBackColor = true;
+            this.btnOut.Click += new System.EventHandler(this.btnOut_Click);
+            // 
+            // txtOut
+            // 
+            this.txtOut.Location = new System.Drawing.Point(103, 62);
+            this.txtOut.Name = "txtOut";
+            this.txtOut.Size = new System.Drawing.Size(298, 21);
+            this.txtOut.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "输出路径";
+            // 
+            // listView
+            // 
+            this.listView.BackColor = System.Drawing.Color.White;
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listView.FullRowSelect = true;
+            this.listView.GridLines = true;
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(0, 129);
+            this.listView.MultiSelect = false;
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(528, 277);
+            this.listView.TabIndex = 6;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "序号";
+            this.columnHeader1.Width = 50;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "文件名称";
+            this.columnHeader2.Width = 250;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "文件大小";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 120;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "状态";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 50;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(422, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "确定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 406);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.btnOut);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnIn);
+            this.Controls.Add(this.txtIn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "窗口";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtIn;
+        private System.Windows.Forms.Button btnIn;
+        private System.Windows.Forms.Button btnOut;
+        private System.Windows.Forms.TextBox txtOut;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+namespace AddYamdiFlv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIn_Click(object sender, EventArgs e)
+        {
+            // 设置根在桌面
+            folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.Desktop;
+
+            // 设置当前选择的路径
+            folderBrowserDialog1.SelectedPath = "C:";
+
+            // 允许在对话框中包括一个新建目录的按钮
+            folderBrowserDialog1.ShowNewFolderButton = true;
+
+            // 设置对话框的说明信息
+            folderBrowserDialog1.Description = "请选择输出目录";
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtIn.Text = folderBrowserDialog1.SelectedPath;
+                getFile(txtIn.Text);
+            }
+        }
+
+        private void btnOut_Click(object sender, EventArgs e)
+        {
+            // 设置根在桌面
+            folderBrowserDialog2.RootFolder = System.Environment.SpecialFolder.Desktop;
+
+            // 设置当前选择的路径
+            folderBrowserDialog2.SelectedPath = "C:";
+
+            // 允许在对话框中包括一个新建目录的按钮
+            folderBrowserDialog2.ShowNewFolderButton = true;
+
+            // 设置对话框的说明信息
+            folderBrowserDialog2.Description = "请选择输出目录";
+
+            if (folderBrowserDialog2.ShowDialog() == DialogResult.OK)
+            {
+                txtOut.Text = folderBrowserDialog2.SelectedPath;
+            }
+        }
+
+        protected void getFile(string paths)
+        {
+            listView.Items.Clear();            
+            DirectoryInfo dirInfo = new DirectoryInfo(paths);
+            int nIndex = 0;
+            foreach (FileSystemInfo fsi in dirInfo.GetFileSystemInfos())
+            {
+                FileInfo fi = (FileInfo)fsi;
+                ListViewItem item = new ListViewItem();
+                item.Text = (nIndex + 1).ToString();
+                item.SubItems.Add(fi.Name);
+                item.SubItems.Add(fi.Length.ToString());
+                item.SubItems.Add("未成功");
+
+                //if (nIndex % 2 == 0)
+                //    item.BackColor = System.Drawing.Color.FromArgb(255, 255, 255);
+                //else
+                //    item.BackColor = System.Drawing.Color.FromArgb(255, 224, 192);
+
+                listView.Items.Add(item);
+                nIndex++;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string paths = System.Windows.Forms.Application.StartupPath + "\\" + "yamdi.exe";
+            for (int i = 0; i < listView.Items.Count; i++)
+            {
+                string arguments = "-i " + txtIn.Text + "\\" + listView.Items[i].SubItems[1].Text + " -o " + txtOut.Text + "\\" + listView.Items[i].SubItems[1].Text + "";
+                if (startapp(paths, arguments))
+                {
+                    listView.Items[i].SubItems[3].Text = "成功";
+                    listView.Items[i].BackColor = System.Drawing.Color.FromArgb(255, 224, 192);
+                }
+                Application.DoEvents();
+            }
+            MessageBox.Show("关键帧添加结束!");
+        }
+
+        /// <summary>  
+        /// 启动外部应用程序，隐藏程序界面  
+        /// </summary>  
+        /// <param name="appname">应用程序路径名称</param>  
+        /// <param name="arguments">启动参数</param>  
+        /// <returns>true表示成功，false表示失败</returns>  
+        public bool startapp(string appname, string arguments)
+        {
+            return startapp(appname, arguments, ProcessWindowStyle.Hidden);
+        }  
+
+        /// <summary>  
+        /// 启动外部应用程序  
+        /// </summary>  
+        /// <param name="appname">应用程序路径名称</param>  
+        /// <param name="arguments">启动参数</param>  
+        /// <param name="style">进程窗口模式</param>  
+        /// <returns>true表示成功，false表示失败</returns>  
+        public bool startapp(string appname, string arguments, ProcessWindowStyle style)
+        {
+            bool blnrst = false;
+            Process p = new Process();
+            p.StartInfo.FileName = appname;//exe,bat and so on  
+            p.StartInfo.WindowStyle = style;
+            p.StartInfo.Arguments = arguments;
+            p.StartInfo.UseShellExecute = true;
+            try
+            {
+                p.Start();
+                p.WaitForExit();
+                p.Close();
+                blnrst = true;
+            }
+            catch
+            {
+            }
+            return blnrst;
+        }  
+    }
+}
